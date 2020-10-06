72a53f27f726955d6d950a536d7d2d630a50268b
Tay Hao Cheng <tayhaocheng.media@gmail.com>
2020-10-07 02:09:24 +0800

    initial commit

---
+++ Assets/Scripts/SmokeMaker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeMaker : MonoBehaviour
+{
+    [SerializeReference]
+    private GameObject smoke = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                smoke.transform.position = hit.point;
+                smoke.GetComponentInChildren<MeshRenderer>().sharedMaterial.SetFloat("_startTime", Time.time);
+            }
+        }
+    }
+}
